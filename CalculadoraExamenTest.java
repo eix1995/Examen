4e7967efed232805c1bf358d50a566bcf0695d43
Gerard <gerard.11@hotmail.es>
2018-04-12 16:13:43 +0200

    Commit segon archiu

---
+++ CalculadoraExamenTest.java
@@ -0,0 +1,29 @@
+package ExamenCaixaNegre;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculadoraExamenTest {
+
+	@Test
+	public void testCalculadoraExamen() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDivideix() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPotencia() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResidu() {
+		fail("Not yet implemented");
+	}
+
+}
